7f0a0af5ef753c3b7b1f60135907e2734ac97928
Wolfgang Kluth <me@kluth.co.uk>
2014-04-30 16:12:11 +0200

    add token based authentication for mobile rest api
    bei jhipster wird der rememberme token benutzt um sich zu
    authentifizieren. er wird normalerweise in den cookies gespeichert, was
    leider bei mobilen Anwendungen nicht sehr hilfreich ist. hiermit gibt
    es die Möglichkeit den Token über den Header mit “Auth-Token”
    einzubinden.
    Den token erhält man zB mit
    curl -i 'http://localhost:8080/app/authentication' --data
    'j_username=zz&j_password=xx&_spring_security_remember_me=true'

--- src/main/java/de/rwth/idsg/brsm/security/CustomPersistentRememberMeServices.java
+++ src/main/java/de/rwth/idsg/brsm/security/CustomPersistentRememberMeServices.java
@@ -80,6 +80,23 @@ public class CustomPersistentRememberMeServices extends
         random = new SecureRandom();
     }
 
+    private final String HEADER_SECURITY_TOKEN = "Auth-Token";
+
+    /**
+     * Locates the Spring Security remember me token in the request and returns its value.
+     *
+     * @param request the submitted request which is to be authenticated
+     * @return the value of the request header (which was originally provided by the cookie - API expects it in header)
+     */
+    @Override protected String extractRememberMeCookie(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_SECURITY_TOKEN);
+        if ((token == null) || (token.length() == 0)) {
+            return null;
+        }
+
+        return token;
+    }
+
     @Override
     @Transactional
     protected UserDetails processAutoLoginCookie(String[] cookieTokens, HttpServletRequest request, HttpServletResponse response) {
