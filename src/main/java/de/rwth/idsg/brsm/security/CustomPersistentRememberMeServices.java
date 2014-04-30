8c33881261dbac11eb226e987fac037223a5f420
Wolfgang Kluth <me@kluth.co.uk>
2014-04-30 16:17:01 +0200

    updated spring boot and add token based auth
    Added token based auth for api access with mobile apps. Add rememberMe
    Token to the header with “Auth-Token”.
    
    How to get rememberMeToken? Login with e.g.:
    
    curl -i 'http://localhost:8080/app/authentication' --data
    'j_username=manager&j_password=user&_spring_security_remember_me=true'

--- src/main/java/de/rwth/idsg/brsm/security/CustomPersistentRememberMeServices.java
+++ src/main/java/de/rwth/idsg/brsm/security/CustomPersistentRememberMeServices.java
@@ -75,9 +75,26 @@ public class CustomPersistentRememberMeServices extends
     @Inject
     public CustomPersistentRememberMeServices(Environment env,
                                               org.springframework.security.core.userdetails.UserDetailsService userDetailsService) {
-    
+
         super(env.getProperty("jhipster.security.rememberme.key"), userDetailsService);
         random = new SecureRandom();
+    }
+
+    private final String HEADER_SECURITY_TOKEN = "Auth-Token";
+
+    /**
+     * Locates the Spring Security remember me token in the request and returns its value.
+     *
+     * @param request the submitted request which is to be authenticated
+     * @return the value of the request header (which was originally provided by the cookie - API expects it in header)
+     */
+    @Override protected String extractRememberMeCookie(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_SECURITY_TOKEN);
+        if ((token == null) || (token.length() == 0)) {
+            return null;
+        }
+
+        return token;
     }
 
     @Override
